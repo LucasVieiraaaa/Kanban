5ac51f42bafd9de34b5fdfa72f8b457971b17d5f
Lucas <lucasvieirasouzaa@gmail.com>
2024-10-26 15:28:14 -0300

    Fixing structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainViewComponent } from './main-view/main-view.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent,
+    MainViewComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModuleModule { }
+export class AppModule { }
