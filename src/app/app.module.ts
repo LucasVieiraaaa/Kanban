a7d4bd4e872b076da693927ac506f93f41dd4612
Lucas <lucasvieirasouzaa@gmail.com>
2024-10-26 16:53:47 -0300

    Adding mat-icon and creating buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './main-view/main-view.component';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -14,8 +15,11 @@ import { MainViewComponent } from './main-view/main-view.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync('noop')
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
