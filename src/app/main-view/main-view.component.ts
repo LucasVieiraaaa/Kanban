47c6d859398748cfb22b29622c3dc55f4603ae1c
Lucas <lucasvieirasouzaa@gmail.com>
2024-10-26 17:25:46 -0300

    Now add buttons are working

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -12,9 +12,34 @@ export class MainViewComponent implements OnInit {
   done: string = "Done";
   addCard: string = "Add Card";
 
+  toDoList: string[] = ['Washe the dishes', 'Clean your room', 'Make biscuts', 'Study for the test']
+  doingList: string[] = ['Update my resume', 'Report the excel ','Submit the excel']
+  doneList: string[] = ['Learn Angular',
+     'Create a repository on GitHub, so I can disclose my programming skills online',
+     'Clean my VSCode cache',
+     'Fix \t"that" bug',
+     'Study about French language, and have a conversation'
+    ]
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addtoDo(){
+    let x = "New Card here, you can edit it"
+    this.toDoList.push(x);
+  }
+  
+  addDoingList(){
+    let x = "New Card here, you can edit it"
+    this.doingList.push(x);
+  }
+
+  addDoneList(){
+    let x = "New Card here, you can edit it"
+    this.doneList.push(x);
   }
 
 }
